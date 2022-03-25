a61ed10b8e721e1128c06efc26e8b106e5e71f90
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-24 19:22:28 -0700

    Update 2022_03_25_021538_create_manufactures_table.php

--- database/migrations/2022_03_25_021538_create_manufactures_table.php
+++ database/migrations/2022_03_25_021538_create_manufactures_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('manufactures', function (Blueprint $table) {
+        Schema::create('manufacturers', function (Blueprint $table) {
             $table->id();
+            
             $table->timestamps();
         });
     }
