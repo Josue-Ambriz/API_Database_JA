311e67169d3e7a90e946bbd71e6d0a4e018ef837
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-24 20:11:14 -0700

    Update 2022_03_25_021641_create_purchases_table.php

--- database/migrations/2022_03_25_021641_create_purchases_table.php
+++ database/migrations/2022_03_25_021641_create_purchases_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('purchases', function (Blueprint $table) {
             $table->id();
-            $table->integer('price');
+            $table->integer('invoice');
             //$table->foreignId('user_id');
             $table->foreignId('hardware_id');
             $table->dateTime('last_purchased');
