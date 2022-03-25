4dba3aaf7ac84b639561f813e208dad8b64da059
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-24 18:49:14 -0700

    Update adminlte.php

--- config/adminlte.php
+++ config/adminlte.php
@@ -238,88 +238,35 @@ return [
 
         // Sidebar items:
         [
-            'type' => 'sidebar-menu-search',
-            'text' => 'search',
+            'type'         => 'sidebar-menu-search',
+            'text'         => 'search',
         ],
+        
         [
-            'text' => 'blog',
-            'url'  => 'admin/blog',
-            'can'  => 'manage-blog',
+            'type' => 'Equipment',
+            'url' => 'equipment',
+            'icon' => 'x',
         ],
+        
         [
-            'text'        => 'pages',
-            'url'         => 'admin/pages',
-            'icon'        => 'far fa-fw fa-file',
-            'label'       => 4,
-            'label_color' => 'success',
+            'text' => 'User Info',
+            'url'  => 'userinfo',
+            'icon' => 'x',
         ],
-        ['header' => 'account_settings'],
+        
+        [         
+            'text' => 'Sales Contact Info',
+            'url'  => 'manufacturer_sales',
+            'icon' => 'x',
+        ],
+        
         [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
-        ],
-        [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
-        ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
-        ],
+            'text' => 'Tech Support',
+            'url'  => 'manufacturer_support',
+            'icon' => 'x',
+        ]
     ],
-
+        
     /*
     |--------------------------------------------------------------------------
     | Menu Filters
