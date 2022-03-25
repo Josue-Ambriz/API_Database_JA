d53c7fd189d37a2fbe2cd893bcb94fd733cd042e
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-24 21:07:22 -0700

    Update Hardware.php

--- app/Models/Hardware.php
+++ app/Models/Hardware.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Hardware extends Model
+{
+    use HasFactory;
+    protected $fillable = ['name', 'cpu', 'ram', 'storage', 'invoice', 'price', 'purchased_on'];
+    protected $table = 'hardware';
+}
