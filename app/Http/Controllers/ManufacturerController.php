38b1e3c6d429da39251d660de81a9403cda226d5
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-25 21:08:34 -0700

    Update ManufacturerController.php

--- app/Http/Controllers/ManufacturerController.php
+++ app/Http/Controllers/ManufacturerController.php
@@ -3,46 +3,95 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Manufacturer;
 
 class ManufacturerController extends Controller
 {
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        //
-    }
+        $events = Calendar::select('title', 'starting AS start', 'ending AS end')->get();
+        return json_encode(compact('events')['events'] );
+    } 
 
-
+   /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
-        //
+        return view('calendars.create');
     }
 
-
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
-        //
+       $validated = $request->validate([ 
+           'title' => 'required',
+           'starting' => 'required',
+           'ending' => 'required',
+        ]);
+        
+       $calendar = Calendar::create([
+            'title' => $request->title,
+            'starting' => date($request->starting),
+            'ending' => date($request->ending), 
+        ]);
+        
+        return redirect('/calendar');
     }
 
-
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function show($id)
     {
-        //
+        $calendar = Calendar::find($id);
+        return view('calendars.show',compact('calendars'));
     }
-
-
+        
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function edit($id)
     {
         //
     }
 
-
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, $id)
     {
         //
     }
 
-
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
         //
