7624391e836abd20e612b25590bae296b82cf828
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-25 22:01:41 -0700

    Update show.blade.php

--- resources/views/manufacturers/show.blade.php
+++ resources/views/manufacturers/show.blade.php
@@ -1,1 +1,17 @@
+@extends('adminlte::page')
 
+@section('title', 'Manufacturer Information')
+
+@section('content_header')
+    <h1>Manufacturer Information</h1>
+@stop
+
+@section('content')
+    <h2>{{ $manufacturer->title; }}</h2>
+    <div><p>{{ $manufacturer->sales_name; }}</p></div>
+    <div><p>{{ $manufacturer->sales_email; }}</p></div>
+    <div><p>{{ $manufacturer->sales_number; }}</p></div>
+    <div><p>{{ $manufacturer->tech_name; }}</p></div>
+    <div><p>{{ $manufacturer->tech_email; }}</p></div>
+    <div><p>{{ $manufacturer->tech_number; }}</p></div>
+@stop
