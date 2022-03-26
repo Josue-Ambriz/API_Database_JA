e69e3b221de326f10b7c7ceafee82ff31151e3ab
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-25 22:13:52 -0700

    Update manufacturers.blade.php

--- resources/views/manufacturers.blade.php
+++ resources/views/manufacturers.blade.php
@@ -1,1 +1,49 @@
+@extends('adminlte::page')
 
+@section('title', 'Manufacturer')
+
+@section('content_header')
+    <h1>Manufacturer Information</h1>
+@stop
+
+@section('content')
+<div class="card">
+  <div class="card-body">
+    <table id="table" class="table table-bordered">
+      <thead>
+        <tr>
+          <th style="width: 10px">#</th><th>sales_name</th><th>sales_email</th><th>sales_number</th><th>tech_name</th><th>tech_email</th><th>tech_number</th>
+        </tr>
+      </thead>
+      <tbody>
+
+        @foreach($manufacturers AS $manufacturer)
+        <tr>
+          <td>{{ $manufacturer->id }}</td>
+          <td>{{ $manufacturer->sales_name }}</td>
+          <td>{{ $manufacturer->sales_email }}</td>
+          <td>{{ $manufacturer->sales_number }}</td>
+          <td>{{ $manufacturer->tech_name }}</td>
+          <td>{{ $manufacturer->tech_email }}</td>
+          <td>{{ $manufacturer->tech_number }}</td>
+          //<td><a class="btn btn-default btn-sm" href="{{ route('manufacturers.show',['manufacturer'=>$manufacturer->id]) }}">View</a></td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+</div>
+<a href="{{ route('manufacturers.create') }} " class="btn btn-primary" >Add</a>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+<script>
+    $(document).ready(function() {
+        $('#table').DataTable();
+    } );
+</script>
+@stop
