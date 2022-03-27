e8bb427b69e958cf0d134ad90c9002ac1d89cf0b
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-27 00:08:01 -0700

    Update create.blade.php

--- resources/views/contactinfos/create.blade.php
+++ resources/views/contactinfos/create.blade.php
@@ -1,1 +1,20 @@
+@extends('adminlte::page')
 
+@section('title', 'Our Contact Information')
+
+@section('content_header')
+    <h1>Contact Information</h1>
+@stop
+
+@section('content')
+<form method="post" action="{{ route('contactinfos.store') }}" >
+    @csrf
+    <x-adminlte-input name="sales_name" label="Sales Name is" />
+    <x-adminlte-input name="sales_email" label="Sales Email is" />
+    <x-adminlte-input name="sales_number" label="Sales Number is" />
+    <x-adminlte-input name="tech_name" label="Tech Support Name" />
+    <x-adminlte-input name="tech_email" label="Tech Support Email" />
+    <x-adminlte-input name="tech_number" label="Tech Support Number" />
+    <x-adminlte-button type="Submit" label="Submit" />
+</form>
+@stop
