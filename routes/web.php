64d3acd4208dd4ae108c81c53f2ba2000a13547e
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-27 00:14:54 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ManufacturerController;
+use App\Http\Controllers\ContactInfoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,6 +26,12 @@ Route::get('/manufacturers', function () {
 });
 
 Route::resource('/manufacturers', ManufacturerController::class);
+
+Route::get('/contactinfos', function () {
+    return view('contactinfos');
+});
+
+Route::resource('/contactinfos', ContactInfoController::class);
 
 
 Route::get('/db-test', function () {
