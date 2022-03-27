949a57225cbe1eb42afdfbcdfacbcd4a45681119
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-27 00:09:15 -0700

    Update show.blade.php

--- resources/views/contactinfos/show.blade.php
+++ resources/views/contactinfos/show.blade.php
@@ -1,17 +1,17 @@
 @extends('adminlte::page')
 
-@section('title', 'Manufacturer Information')
+@section('title', 'Contact Information')
 
 @section('content_header')
-    <h1>Manufacturer Information</h1>
+    <h1>Contact Information</h1>
 @stop
 
 @section('content')
-    <h2>{{ $manufacturer->title; }}</h2>
-    <div><p>{{ $manufacturer->sales_name; }}</p></div>
-    <div><p>{{ $manufacturer->sales_email; }}</p></div>
-    <div><p>{{ $manufacturer->sales_number; }}</p></div>
-    <div><p>{{ $manufacturer->tech_name; }}</p></div>
-    <div><p>{{ $manufacturer->tech_email; }}</p></div>
-    <div><p>{{ $manufacturer->tech_number; }}</p></div>
+    <h2>{{ $contactinfo->title; }}</h2>
+    <div><p>{{ $contactinfo->sales_name; }}</p></div>
+    <div><p>{{ $contactinfo->sales_email; }}</p></div>
+    <div><p>{{ $contactinfo->sales_number; }}</p></div>
+    <div><p>{{ $contactinfo->tech_name; }}</p></div>
+    <div><p>{{ $contactinfo->tech_email; }}</p></div>
+    <div><p>{{ $contactinfo->tech_number; }}</p></div>
 @stop
