f359059e398b44faea6e8d513cfd3247f330bf6b
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-26 23:52:41 -0700

    Update UserInfo.php

--- app/Models/UserInfo.php
+++ app/Models/UserInfo.php
@@ -4,9 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Hardware;
+use App\Models\Purchase;
 
 class UserInfo extends Model
 {
     protected $fillable = ['first_name','last_name', 'email', 'number'];
     use HasFactory;
+    
+    public function purchase()
+    {
+        return $this->hasMany(Purchase::class);
+    }
+
+    public function hardware()
+    {
+        return $this->hasMany(Hardware::class);
+    }
 }
