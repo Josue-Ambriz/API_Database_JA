7672a53ca1f67f3749482dd4eba30238fb39021f
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-26 23:48:52 -0700

    Update Purchase.php

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -4,11 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App/Models/UserInfo;
+use App/Models/Hardware;
 
 class Purchase extends Model
 {
     use HasFactory;
-    protected $fillable = ['sales_name', 'sales_email', 'sales_number', 'tech_name', 'tech_email', 'tech_number'];
-    protected $table = 'manufacturers';
+    protected $fillable = ['invoice', 'price', 'purchased_on';
+   
+   public function userinfo()
+   {
+       return $this->belongsTo(UserInfo::class);
+   }
+                           
+    public function hardware()
+    {
+        return $this->hasOne(Hardware::class);
+    }
 }
-
