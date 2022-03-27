0ddadbcb4cbfa520655c2cc503a3e1c5635174ac
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-26 23:28:27 -0700

    Update Hardware.php

--- app/Models/Hardware.php
+++ app/Models/Hardware.php
@@ -5,13 +5,34 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App/Models/HWCategory;
+use App/Models/Manufacturer;
+use App/Models/Purchase;
+use App/Models/UserInfo;
 
 class Hardware extends Model
 {
     use HasFactory;
     use SoftDeletes;
-    protected $fillable = ['name', 'cpu', 'ram', 'storage', 'service', 'software', 'price'];
+    protected $fillable = ['name', 'hwcategory_id', 'cpu', 'ram', 'storage', 'service', 'software', 'price', 'manufacturer_id'];
     
-    public function 
+    public function hwcategory()
+    {
+        return $this->belongsTo(HWCategory::class);
+    }
     
+    public function manufacturer()
+    {
+        return $this->belongsTo(Manufacturer::class);
+    }
+    
+    public function purchase()
+    {
+        return $this->hasOne(Purchase::class);
+    }
+    
+    public function equipment()
+    {
+        return $this->hasMany(Equipment::class);
+    }
 }
