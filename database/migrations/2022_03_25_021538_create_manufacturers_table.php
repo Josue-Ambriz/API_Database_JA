161ad177f08ba51a8806607a19e1b6449dcf9ccb
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-27 00:18:52 -0700

    Update 2022_03_25_021538_create_manufacturers_table.php

--- database/migrations/2022_03_25_021538_create_manufacturers_table.php
+++ database/migrations/2022_03_25_021538_create_manufacturers_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->string('tech_number');
             $table->softDeletes();
             $table->timestamps();
-        };
+        });
     } 
 
     /**
