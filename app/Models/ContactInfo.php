eb69506a6df4cf3fbd34b59e3930825f66002a57
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-26 23:02:02 -0700

    Update ContactInfo.php

--- app/Models/ContactInfo.php
+++ app/Models/ContactInfo.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Manufacturer extends Model
+{
+    use HasFactory;
+    protected $fillable = ['sales_name', 'sales_email', 'sales_number', 'tech_name', 'tech_email', 'tech_number'];
+    protected $table = 'manufacturers';
+}
