eeffe36ef76f20e9c3af8b7a5479c971bc0a7805
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-26 23:53:33 -0700

    needs to be updated

--- app/Models/Manufacturer.php
+++ app/Models/Manufacturer.php
@@ -11,3 +11,4 @@ class Manufacturer extends Model
     protected $fillable = ['sales_name', 'sales_email', 'sales_number', 'tech_name', 'tech_email', 'tech_number'];
     protected $table = 'manufacturers';
 }
+kkk
