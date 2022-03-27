e479a0dcd831afa60724f35918c3df91545e73cc
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-26 23:55:38 -0700

    Update ContactInfo.php

--- app/Models/ContactInfo.php
+++ app/Models/ContactInfo.php
@@ -5,10 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Hardware;
 
 class ContactInfo extends Model
 {
     use HasFactory;
     use SoftDeletes;
     protected $fillable = ['sales_name', 'sales_email', 'sales_number', 'tech_name', 'tech_email', 'tech_number'];
+    
+    public function hardware()
+    {
+        return $this->hasMany(Hardware::class);
+    }
 }
