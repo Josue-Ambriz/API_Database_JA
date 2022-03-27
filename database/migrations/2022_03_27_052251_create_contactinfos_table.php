aa3c9f75ba5d2d118226ab9e6fee06c7a56cdf63
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-26 22:37:18 -0700

    Update 2022_03_27_052251_create_contactinfos_table.php

--- database/migrations/2022_03_27_052251_create_contactinfos_table.php
+++ database/migrations/2022_03_27_052251_create_contactinfos_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('contactinfos', function (Blueprint $table) {
             $table->id();
+            $table->string('sales_name');
+            $table->string('sales_email');
+            $table->string('sales_number');
+            $table->string('tech_name');
+            $table->string('tech_email');
+            $table->string('tech_number');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
