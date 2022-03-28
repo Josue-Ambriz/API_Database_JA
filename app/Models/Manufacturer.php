a5db719144f91584796d8274d1cffb4bd542e82b
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-28 13:54:59 -0700

    Update Manufacturer.php

--- app/Models/Manufacturer.php
+++ app/Models/Manufacturer.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Hardware;
 
 class Manufacturer extends Model
 {
     use HasFactory;
     protected $fillable = ['company', 'sales_email', 'sales_number', 'tech_email', 'tech_number'];
-    protected $table = 'manufacturers';
+}
+
+public function hardware()
+{
+    return $this->hasMany(Hardware::class);
 }
