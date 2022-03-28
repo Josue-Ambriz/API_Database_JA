2f81b4a765a57b44c4cce1ce5ccddec78884769f
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-27 17:13:03 -0700

    Update HardwareController.php

--- app/Http/Controllers/HardwareController.php
+++ app/Http/Controllers/HardwareController.php
@@ -7,43 +7,97 @@ use App\Models\Hardware;
 
 class HardwareController extends Controller
 {
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        //
+        $manufacturers = Manufacturer::all();
+        return view('manufacturers', compact('manufacturers'));
     }
 
-
+   /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
-        //
+        return view('manufacturers.create');
     }
 
-
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(Request $request)
     {
-        //
+       $validated = $request->validate([ 
+           'sales_name' => 'required',
+           'sales_email' => 'required',
+           'sales_number' => 'required',
+           'tech_name' => 'required',
+           'tech_email' => 'required',
+           'tech_number' => 'required',
+        ]);
+        
+       $manufacturer = Manufacturer::create([
+            'sales_name' => $request->sales_name,
+            'sales_email' => $request->sales_email,
+            'sales_number' => $request->sales_number,
+            'tech_name' => $request->tech_name,
+            'tech_email' => $request->tech_email,
+            'tech_number' => $request->tech_number,
+        ]);
+        
+        return $this->index();
     }
 
-
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function show($id)
     {
-        //
+        $manufacturer = Manufacturer::find($id);
+        return view('manufacturers.show',compact('manufacturer'));
     }
-
-
+        
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function edit($id)
     {
         //
     }
 
-
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, $id)
     {
         //
     }
 
-
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
         //
