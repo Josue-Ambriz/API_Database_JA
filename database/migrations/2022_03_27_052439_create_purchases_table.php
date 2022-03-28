0b6b0288530284cedae8c0eb1850e8ab90a7899e
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-28 12:46:54 -0700

    Update 2022_03_27_052439_create_purchases_table.php

--- database/migrations/2022_03_27_052439_create_purchases_table.php
+++ database/migrations/2022_03_27_052439_create_purchases_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('invoice');
             $table->integer('price');
-            $table->dateTime('purchased_on');
+            $table->date('purchased_on');
             $table->timestamps();
         });
     }
