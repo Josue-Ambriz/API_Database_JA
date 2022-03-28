454c63e91f04d199228391c2eaf07c9b3e6fbeac
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-28 12:50:51 -0700

    Update 2022_03_27_052251_create_contactinfos_table.php

--- database/migrations/2022_03_27_052251_create_contactinfos_table.php
+++ database/migrations/2022_03_27_052251_create_contactinfos_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->string('tech_name');
             $table->string('tech_email');
             $table->string('tech_number');
-            $table->softDeletes();
             $table->timestamps();
         });
     }
