3cabe8c5c9db58f2e0c120545be9c92b962f749e
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-27 18:15:50 -0700

    Update hardwares.blade.php

--- resources/views/hardwares.blade.php
+++ resources/views/hardwares.blade.php
@@ -12,20 +12,23 @@
     <table id="table" class="table table-bordered">
       <thead>
         <tr>
-          <th style="width: 10px">#</th><th>Sales Name</th><th>Sales Email</th><th>Sales Number</th><th>Tech Name</th><th>Tech Email</th><th>Tech Number</th>
+          <th style="width: 10px">#</th><th>Name</th><th>Category</th><th>CPU</th><th>RAM</th><th>Storage</th><th>Service</th><th>Software</th><th>Price</th><th>Manufacturer</th>
         </tr>
       </thead>
       <tbody>
 
-        @foreach($contactinfos AS $contactinfo)
+        @foreach($hardwares AS $hardware)
         <tr>
-          <td>{{ $contactinfo->id }}</td>
-          <td>{{ $contactinfo->sales_name }}</td>
-          <td>{{ $contactinfo->sales_email }}</td>
-          <td>{{ $contactinfo->sales_number }}</td>
-          <td>{{ $contactinfo->tech_name }}</td>
-          <td>{{ $contactinfo->tech_email }}</td>
-          <td>{{ $contactinfo->tech_number }}</td>
+          <td>{{ $hardware->id }}</td>
+          <td>{{ $hardware->name }}</td>
+          <td>{{ $hardware->hwcategory_id }}</td>
+          <td>{{ $hardware->cpu }}</td>
+          <td>{{ $hardware->ram }}</td>
+          <td>{{ $hardware->storage }}</td>
+          <td>{{ $hardware->service }}</td>
+          <td>{{ $hardware->software }}</td>
+          <td>{{ $hardware->price }}</td>
+          <td>{{ $hardware->manufacturer_id }}</td>
         </tr>
         @endforeach
       </tbody>
