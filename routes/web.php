b4e28b7b3e5fad22eef8d55b9d03e46b37b1ea8b
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-27 18:21:19 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ManufacturerController;
 use App\Http\Controllers\ContactInfoController;
+use App\Http\Controllers\HardwareController;
 
 /*
 |--------------------------------------------------------------------------
