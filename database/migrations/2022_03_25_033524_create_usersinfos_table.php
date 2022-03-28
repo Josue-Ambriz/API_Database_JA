9da4703c54dcd4803c873389ae3656620f41e54d
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-28 12:42:02 -0700

    Update 2022_03_25_033524_create_usersinfos_table.php

--- database/migrations/2022_03_25_033524_create_usersinfos_table.php
+++ database/migrations/2022_03_25_033524_create_usersinfos_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('email');
             $table->integer('number');
-            $table->softDeletes();
             $table->timestamps();
         });
     }
