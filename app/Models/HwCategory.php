cbf7820fcbb3eb59c3790d0b168e95fedbe3fcb0
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 15:38:46 -0700

    Update and rename HWCategory.php to HwCategory.php

--- app/Models/HwCategory.php
+++ app/Models/HwCategory.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Hardware;
 
-class HWCategory extends Model
+class HwCategory extends Model
 {
     use HasFactory;
     protected $fillable = ['type'];
