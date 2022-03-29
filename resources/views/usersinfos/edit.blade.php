d312f5f589887cfb887d667872567eeaf8ca9785
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 12:43:09 -0700

    Create edit.blade.php

---
+++ resources/views/usersinfos/edit.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Edit User Information')
+
+@section('content_header')
+  <h1>Edit User Information</h1>
+@stop
+
+@section('content')
+<form method="post" action="{{ route('usersinfos.update', ['userinfo'=>$userinfo->id]) }}">
+  @csrf
+  @method('PUT')
+  <x-adminlte-input name="first_name" value="{{ $userinfo->first_name }}" label="Edit First Name" />
+  <x-adminlte-input name="last_name" value="{{ $userinfo->last_name }}" label="Edit Last Name" />
+  <x-adminlte-input name="email" value="{{ $userinfo->email }}" label="Edit Email" />
+  <x-adminlte-input name="number" value="{{ $userinfo->number }}" label="Edit Number" />
+  <x-adminlte-button type="Submit" label="Submit" />
+</form>
+@stop
