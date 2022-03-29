87a2193fe780f16307735a93f61e5574473ada65
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 13:06:09 -0700

    Create create.blade.php

---
+++ resources/views/notes/create.blade.php
@@ -0,0 +1,17 @@
+@extends('adminlte::page')
+
+@section('title', 'Add New Note')
+
+@section('content_header')
+    <h1>Add New Note</h1>
+@stop
+
+@section('content')
+<form method="post" action="{{ route('notes.store') }}" >
+    @csrf
+    <x-adminlte-input name="note" label="Note" />
+    <x-adminlte-input name="hardware_id" label="Hardware ID" />
+    <x-adminlte-input name="service" label="Service" />
+    <x-adminlte-button type="Submit" label="Submit" />
+</form>
+@stop
