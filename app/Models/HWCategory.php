bc47a78c7ca2300c9dacfe7885c2761d2cfbdf23
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 15:57:32 -0700

    Update HWCategory.php

--- app/Models/HWCategory.php
+++ app/Models/HWCategory.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Hardware;
 
-class HwCategory extends Model
+class HWCategory extends Model
 {
     use HasFactory;
     protected $fillable = ['type'];
