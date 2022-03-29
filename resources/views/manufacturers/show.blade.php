8be3c5faf97e4b81b468c3a33da44a5b25ce44f2
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-28 19:47:06 -0700

    Update show.blade.php

--- resources/views/manufacturers/show.blade.php
+++ resources/views/manufacturers/show.blade.php
@@ -13,4 +13,22 @@
     <div><p>{{ $manufacturer->sales_number; }}</p></div>
     <div><p>{{ $manufacturer->tech_email; }}</p></div>
     <div><p>{{ $manufacturer->tech_number; }}</p></div>
+    
+    <h4><div><a href="{{route($n.'.edit', [(string)$m=>$res->id]) }}" class="btn btn-primary" >Edit</a>
+        <form style="display:inline;" class="delete" action="{{route($n.'.destroy', [(string)$m=>$res->id])}}" method="post">
+    @method('delete')
+    @csrf
+    <input type="hidden" name="_method" value="DELETE">
+    <input type="submit" value="Delete {{ $o; }}" class="btn btn-danger">
+  </form>
+</div></h4>
+@stop
+
+@section('js')
+<script>
+    $(".delete").on("submit", function(){
+      return confirm("Are you sure?");
+    });
+</script>
+    
 @stop
