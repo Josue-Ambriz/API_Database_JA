adc8cbd39cc3f582d41152601a4bf4617bab9161
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 00:11:14 -0700

    Update edit.blade.php

--- resources/views/contactinfos/edit.blade.php
+++ resources/views/contactinfos/edit.blade.php
@@ -1,1 +1,18 @@
+@extends('adminlte::page')
 
+@section('title', 'Edit Website Information')
+
+@section('content_header')
+  <h1>Edit Website Information</h1>
+@stop
+
+@section('content')
+<form method="post" action="{{ route('contactinfos.update', ['contactinfo'=>$contactinfo->id]) }}">
+  @csrf
+  @method('PUT')
+  <x-adminlte-input name="owner" value="{{ $contactinfo->owner }}" label="Name of Owner" />
+  <x-adminlte-input name="info" value="{{ $contactinfo->sales_email }}" label="Name of Organization" />
+  <x-adminlte-input name="email" value="{{ $contactinfo->sales_number }}" label="Email" />
+  <x-adminlte-button type="Submit" label="Submit" />
+</form>
+@stop
