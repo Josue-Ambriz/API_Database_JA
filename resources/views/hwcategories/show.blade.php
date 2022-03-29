c3f9a27c20afa7d2d263afc95eec2ff517b1c8a7
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-28 23:09:05 -0700

    Update show.blade.php

--- resources/views/hwcategories/show.blade.php
+++ resources/views/hwcategories/show.blade.php
@@ -1,1 +1,24 @@
+@extends('adminlte::page')
 
+@section('title', 'Category Information')
+
+@section('content_header')
+    <h1>Category Information</h1>
+@stop
+
+@section('content')
+    <h2>{{ $hwcategory->title; }}</h2>
+    <div><p>{{ $hwcategory->type; }}</p></div>
+    
+    <h4>
+        <div>
+            <a href="{{route('hwcategories.edit', ['manufacturer'=>$hwcategory->id]) }}" class="btn btn-primary" >Edit</a>
+            @csrf
+            <form style="display:inline;" class="delete" action="{{route('hwcategories.destroy', ['hwcategory'=>$hwcategory->id])}}" method="post">
+            @method('delete')
+            <button type="submit" class="btn btn-danger">Delete</button>
+            @csrf
+            </form>
+        </div>
+    </h4>
+@stop
