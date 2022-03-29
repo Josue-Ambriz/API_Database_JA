6ba4678f0658201ba3b4598615f894a936ea12b2
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 01:32:44 -0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <p>Hello fellow vistor, welcome to RedzBluez Electronic Co. This website allows access to the latest technology that we have to offer. Whether by our own products, or by fellow manufacturers that allow us to sell their products here as well. Enjoy your stay!</p>
 @stop
 
 @section('css')
