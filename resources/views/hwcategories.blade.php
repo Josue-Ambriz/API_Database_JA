5f73b368c96601e1ed5ae43df41af084e3877fd6
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-28 23:12:01 -0700

    Update hwcategories.blade.php

--- resources/views/hwcategories.blade.php
+++ resources/views/hwcategories.blade.php
@@ -1,1 +1,44 @@
+@extends('adminlte::page')
 
+@section('title', 'Hardware Category')
+
+@section('content_header')
+    <h1>Hardware Category</h1>
+@stop
+
+@section('content')
+<div class="card">
+  <div class="card-body">
+    <table id="table" class="table table-bordered">
+      <thead>
+        <tr>
+          <th style="width: 10px">#</th><th>Type of Category</th>
+        </tr>
+      </thead>
+      <tbody>
+
+        @foreach($hwcategories AS $hwcategory)
+        <tr>
+          <td>{{ $hwcategory->id }}</td>
+          <td>{{ $hwcategory->type }}</td>
+          <td><a class="btn btn-default btn-sm" href="{{ route('hwcategories.show',['hwcategory'=>$hwcategory->id]) }}">View</a></td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+</div>
+<a href="{{ route('hwcategories.create') }} " class="btn btn-primary" >Add</a>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+<script>
+    $(document).ready(function() {
+        $('#table').DataTable();
+    } );
+</script>
+@stop
