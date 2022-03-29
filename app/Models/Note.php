1a22fffd0db95f7b04d0898be91dd2c034494afa
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 14:13:17 -0700

    Update Note.php

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -4,15 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Hardware;
+use App\Models\Purchase;
 
 class Note extends Model
 {
     use HasFactory;
-    protected $fillable = ['note', 'hardware_id', 'service'];
+    protected $fillable = ['note', 'purchase_id', 'service'];
 
-    public function hardware()
+    public function purchase()
     {
-        return $this->belongsTo(Hardware::class);
+        return $this->belongsTo(Purchase::class);
     }
 }
