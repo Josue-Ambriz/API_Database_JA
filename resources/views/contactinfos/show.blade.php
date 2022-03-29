6ca2c136424eb48ad7c906a807cb75e57eb938d9
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 00:17:53 -0700

    Update show.blade.php

--- resources/views/contactinfos/show.blade.php
+++ resources/views/contactinfos/show.blade.php
@@ -11,4 +11,16 @@
     <div><p>{{ $contactinfo->owner; }}</p></div>
     <div><p>{{ $contactinfo->info; }}</p></div>
     <div><p>{{ $contactinfo->email; }}</p></div>
+    
+   <h4>
+        <div>
+            <a href="{{route('contactinfos.edit', ['contactinfo'=>$contactinfo->id]) }}" class="btn btn-primary" >Edit</a>
+            @csrf
+            <form style="display:inline;" class="delete" action="{{route('contactinfos.destroy', ['contactinfo'=>$contactinfo->id])}}" method="post">
+            @method('delete')
+            <button type="submit" class="btn btn-danger">Delete</button>
+            @csrf
+            </form>
+        </div>
+    </h4>
 @stop
