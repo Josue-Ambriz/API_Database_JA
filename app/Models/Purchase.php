37e2ab75f2ec8e01bfb6b91fdf082620d8d38d4d
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 16:09:20 -0700

    Update Purchase.php

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -4,8 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\UserInfo;
 use App\Models\Hardware;
+use App\Models\UserInfo;
 use App\Models\Note;
 
 class Purchase extends Model
