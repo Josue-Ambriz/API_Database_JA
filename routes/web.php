62ac3f39c23c23580bbf5af1e25f8cfc8d57017a
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 13:18:47 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HardwareController;
-use App\Http\Controllers\ManufacturerController;
 use App\Http\Controllers\HWCategoryController;
+use App\Http\Controllers\ManufacturerController;
+use App\Http\Controllers\HardwareController;
+use App\Http\Controllers\UserInfoController;
+use App\Http\Controllers\PurchaseController;
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\ContactInfoController;
 
 /*
@@ -23,16 +26,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hardwares', function () {
-    return view('hardwares');
+Route::get('/hwcategories', function () {
+    return view('hwcategories');
 });
 
 Route::get('/manufacturers', function () {
     return view('manufacturers');
 });
 
-Route::get('/hwcategories', function () {
-    return view('hwcategories');
+
+Route::get('/hardwares', function () {
+    return view('hardwares');
+});
+
+Route::get('/usersinfos', function () {
+    return view('usersinfos');
+});
+
+Route::get('/purchases', function () {
+    return view('purchases');
+});
+
+Route::get('/notes', function () {
+    return view('notes');
 });
 
 Route::get('/contactinfos', function () {
@@ -57,7 +73,10 @@ Route::get('/db-migrate-refresh', function () {
     echo Artisan::output();
 });
 
-Route::resource('/hardwares', HardwareController::class);
-Route::resource('/manufacturers', ManufacturerController::class);
 Route::resource('/hwcategories', HWCategoryController::class);
+Route::resource('/manufacturers', ManufacturerController::class);
+Route::resource('/hardwares', HardwareController::class);
+Route::resource('/usersinfos', UserInfoController::class);
+Route::resource('/purchases', PurchaseController::class);
+Route::resource('/notes', NoteController::class);
 Route::resource('/contactinfos', ContactInfoController::class);
