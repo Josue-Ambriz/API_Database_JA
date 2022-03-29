84b2affec46b5396a627c8900dca9eec7467cf91
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 12:45:13 -0700

    Create show.blade.php

---
+++ resources/views/usersinfos/show.blade.php
@@ -0,0 +1,27 @@
+@extends('adminlte::page')
+
+@section('title', 'User Information')
+
+@section('content_header')
+    <h1>User Information</h1>
+@stop
+
+@section('content')
+    <h2>{{ $userinfo->title; }}</h2>
+    <div><p>{{ $userinfo->first_name; }}</p></div>
+    <div><p>{{ $userinfo->last_name; }}</p></div>
+    <div><p>{{ $userinfo->email; }}</p></div>
+    <div><p>{{ $userinfo->number; }}</p></div>
+    
+    <h4>
+        <div>
+            <a href="{{route('usersinfos.edit', ['userinfo'=>$userinfo->id]) }}" class="btn btn-primary" >Edit</a>
+            @csrf
+            <form style="display:inline;" class="delete" action="{{route('usersinfos.destroy', ['userinfo'=>$userinfo->id])}}" method="post">
+            @method('delete')
+            <button type="submit" class="btn btn-danger">Delete</button>
+            @csrf
+            </form>
+        </div>
+    </h4>
+@stop
