c07262a0eb499e60ce77eb6373d03b3ad0f6569d
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-28 19:55:57 -0700

    Update show.blade.php

--- resources/views/manufacturers/show.blade.php
+++ resources/views/manufacturers/show.blade.php
@@ -14,21 +14,18 @@
     <div><p>{{ $manufacturer->tech_email; }}</p></div>
     <div><p>{{ $manufacturer->tech_number; }}</p></div>
     
-    <h4><div><a href="{{route($n.'.edit', [(string)$m=>$res->id]) }}" class="btn btn-primary" >Edit</a>
-        <form style="display:inline;" class="delete" action="{{route($n.'.destroy', [(string)$m=>$res->id])}}" method="post">
+    <h4><div><a href="{{route('manufacturers.edit', ['manufacturer'=>$manufacturer->id]) }}" class="btn btn-primary" >Edit</a>
+    
+    <form style="display:inline;" class="delete" action="{{route($n.'.destroy', [(string)$m=>$res->id])}}" method="post">
+    
     @method('delete')
+    
     @csrf
-    <input type="hidden" name="_method" value="DELETE">
-    <input type="submit" value="Delete {{ $o; }}" class="btn btn-danger">
+    
+    <button type="submit" class="btn btn-danger">Delete</button>
+    
   </form>
 </div></h4>
 @stop
-
-@section('js')
-<script>
-    $(".delete").on("submit", function(){
-      return confirm("Are you sure?");
-    });
-</script>
     
 @stop
