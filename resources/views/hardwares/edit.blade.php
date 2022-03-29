a644cc8de8711843870b8b3cf9276a42f5637397
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-28 21:22:52 -0700

    Update edit.blade.php

--- resources/views/hardwares/edit.blade.php
+++ resources/views/hardwares/edit.blade.php
@@ -13,7 +13,7 @@
   <x-adminlte-input name="name" value="{{ $hardware->name }}" label="Name" />
   <x-adminlte-input name="hwcategory_id" value="{{ $hardware->hwcategory_id }}" label="Category" />
   <x-adminlte-input name="cpu" value="{{ $hardware->cpu }}" label="CPU" />
-  <x-adminlte-input name="ram" value="{{ $hardware->ram }}" label="Ram" />
+  <x-adminlte-input name="ram" value="{{ $hardware->ram }}" label="RAM" />
   <x-adminlte-input name="storage" value="{{ $hardware->storage }}"  label="Storage" />
   <x-adminlte-input name="software" value="{{ $hardware->software }}" label="Software" />
   <x-adminlte-input name="manufacturer_id" value="{{ $hardware->manufacturer_id }}"  label="Manufacturer" />
