e6fcb3b70dd72d9a3f13fc9c67e175138c72ba7e
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 14:07:58 -0700

    Update 2022_03_27_194435_create_notes_table.php

--- database/migrations/2022_03_27_194435_create_notes_table.php
+++ database/migrations/2022_03_27_194435_create_notes_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
             $table->text('note');
-            $table->foreignId('hardware_id')->constrained('hardwares');
+            $table->foreignId('userinfo_id')->constrained('usersinfos');
             $table->string('service');
             $table->timestamps();
         });
