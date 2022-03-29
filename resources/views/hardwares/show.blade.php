3ea3f89740d8407f260e2effffaffe2aff91f8e2
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-28 21:24:56 -0700

    Update show.blade.php

--- resources/views/hardwares/show.blade.php
+++ resources/views/hardwares/show.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Hardware')
 
 @section('content_header')
-    <h1>Hardware Has Been Added</h1>
+    <h1>Hardware</h1>
 @stop
 
 @section('content')
@@ -13,8 +13,18 @@
     <div><p>{{ $hardware->cpu; }}</p></div>
     <div><p>{{ $hardware->ram; }}</p></div>
     <div><p>{{ $hardware->storage; }}</p></div>
-    <div><p>{{ $hardware->service; }}</p></div>
     <div><p>{{ $hardware->software; }}</p></div>
-    <div><p>{{ $hardware->price; }}</p></div>
     <div><p>{{ $hardware->manufacturer_id; }}</p></div>
+    
+    <h4>
+        <div>
+            <a href="{{route('hardwares.edit', ['hardware'=>$hardware->id]) }}" class="btn btn-primary" >Edit</a>
+            @csrf
+            <form style="display:inline;" class="delete" action="{{route('hardwares.destroy', ['hardware'=>$hardware->id])}}" method="post">
+            @method('delete')
+            <button type="submit" class="btn btn-danger">Delete</button>
+            @csrf
+            </form>
+        </div>
+    </h4>
 @stop
