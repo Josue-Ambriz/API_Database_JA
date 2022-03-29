726d9dbd114dfa7145025c756312482548de8eeb
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-28 23:07:17 -0700

    Update edit.blade.php

--- resources/views/hwcategories/edit.blade.php
+++ resources/views/hwcategories/edit.blade.php
@@ -1,1 +1,16 @@
+@extends('adminlte::page')
 
+@section('title', 'Edit Category')
+
+@section('content_header')
+  <h1>Edit Category</h1>
+@stop
+
+@section('content')
+<form method="post" action="{{ route('hwcategories.update', ['hwcategory'=>$hwcategory->id]) }}">
+  @csrf
+  @method('PUT')
+  <x-adminlte-input name="type" value="{{ $hwcategory->type }}" label="Type of Category" />
+  <x-adminlte-button type="Submit" label="Submit" />
+</form>
+@stop
