2da772b72197de448f56acf4c836c29f08e3c554
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-28 23:05:42 -0700

    Update create.blade.php

--- resources/views/hwcategories/create.blade.php
+++ resources/views/hwcategories/create.blade.php
@@ -1,1 +1,15 @@
+@extends('adminlte::page')
 
+@section('title', 'Add Category')
+
+@section('content_header')
+    <h1>Add Category</h1>
+@stop
+
+@section('content')
+<form method="post" action="{{ route('hwcategories.store') }}" >
+    @csrf
+    <x-adminlte-input name="type" label="Type of Category" />
+    <x-adminlte-button type="Submit" label="Submit" />
+</form>
+@stop
