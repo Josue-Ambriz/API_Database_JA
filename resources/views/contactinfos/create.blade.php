6fe5b119952d49a8257ba1adfdc1b75007e8b89d
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 20:10:10 -0700

    Update create.blade.php

--- resources/views/contactinfos/create.blade.php
+++ resources/views/contactinfos/create.blade.php
@@ -12,6 +12,7 @@
     <x-adminlte-input name="owner" label="Name of Owner" />
     <x-adminlte-input name="info" label="Name of Organization" />
     <x-adminlte-input name="email" label="Email" />
+    <x-adminlte-input name="note_id" label="Total Notes" />
     <x-adminlte-button type="Submit" label="Submit" />
 </form>
 @stop
