0b998f1ca541fdac14905722960357fa95130ecb
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 21:35:17 -0700

    Update ContactInfoController.php

--- app/Http/Controllers/ContactInfoController.php
+++ app/Http/Controllers/ContactInfoController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\ContactInfo;
 use App\Models\Note;
 
-class ContactInfoController extends Controller
+class contactinfoController extends Controller
 {
     /**
      * Display a listing of the resource.
