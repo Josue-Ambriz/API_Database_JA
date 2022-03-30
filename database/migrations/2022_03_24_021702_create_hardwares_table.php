f2d4143c6874d1e2963b08b32a11e6cdba0f4a2d
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 19:32:50 -0700

    Update 2022_03_24_021702_create_hardwares_table.php

--- database/migrations/2022_03_24_021702_create_hardwares_table.php
+++ database/migrations/2022_03_24_021702_create_hardwares_table.php
@@ -16,12 +16,12 @@ return new class extends Migration
         Schema::create('hardwares', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->foreignId('hwcategory_id')->constrained('hwcategories');
+            $table->foreignId('hwcategory_id')->constrained('hwcategories')->onDelete('cascade');
             $table->string('cpu');
             $table->string('ram');
             $table->string('storage');
             $table->string('software');
-            $table->foreignId('manufacturer_id')->constrained('manufacturers');
+            $table->foreignId('manufacturer_id')->constrained('manufacturers')->onDelete('cascade');
             $table->timestamps();
         });
     }
