57b73240b71f4f233d984150fad2dc806950e064
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 20:11:43 -0700

    Update edit.blade.php

--- resources/views/contactinfos/edit.blade.php
+++ resources/views/contactinfos/edit.blade.php
@@ -13,6 +13,7 @@
   <x-adminlte-input name="owner" value="{{ $contactinfo->owner }}" label="Name of Owner" />
   <x-adminlte-input name="info" value="{{ $contactinfo->info }}" label="Name of Organization" />
   <x-adminlte-input name="email" value="{{ $contactinfo->email }}" label="Email" />
+  <x-adminlte-input name="note_id" value="{{ $contactinfo->note_id }}" label="Edit Total Notes" />
   <x-adminlte-button type="Submit" label="Submit" />
 </form>
 @stop
