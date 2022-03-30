b5e6056b1cbdf0d5d0b341ac3ce6710a2b4f7171
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-30 16:36:18 -0700

    Update contactinfos.blade.php

--- resources/views/contactinfos.blade.php
+++ resources/views/contactinfos.blade.php
@@ -12,7 +12,7 @@
     <table id="table" class="table table-bordered">
       <thead>
         <tr>
-          <th style="width: 10px">#</th><th>Owner</th><th>Information</th><th>Email</th>
+          <th style="width: 10px">#</th><th>Owner</th><th>Information</th><th>Email</th><th>Total Notes</th>
         </tr>
       </thead>
       <tbody>
