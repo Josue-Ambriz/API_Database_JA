65c99d3868279bb083d2e484a1cf5c3292400b25
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 22:26:10 -0700

    Update and rename manufacturerController.php to ManufacturerController.php

--- app/Http/Controllers/ManufacturerController.php
+++ app/Http/Controllers/ManufacturerController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Manufacturer;
 
-class manufacturerController extends Controller
+class ManufacturerController extends Controller
 {
     /**
      * Display a listing of the resource.
