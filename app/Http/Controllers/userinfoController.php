e583f08d33134dbf71df71ed798d7c3be238bfcf
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 21:51:58 -0700

    Update userinfoController.php

--- app/Http/Controllers/userinfoController.php
+++ app/Http/Controllers/userinfoController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\UserInfo;
 
-class UserInfoController extends Controller
+class userinfoController extends Controller
 {
     /**
      * Display a listing of the resource.
