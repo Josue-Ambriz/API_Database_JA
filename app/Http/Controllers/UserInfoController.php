2df6d96740eb082baec4583cb4be7649a709ff11
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 22:26:56 -0700

    Update and rename userinfoController.php to UserInfoController.php

--- app/Http/Controllers/UserInfoController.php
+++ app/Http/Controllers/UserInfoController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\UserInfo;
 
-class userinfoController extends Controller
+class UserInfoController extends Controller
 {
     /**
      * Display a listing of the resource.
