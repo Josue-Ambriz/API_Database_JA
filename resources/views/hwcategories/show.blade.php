01f40f219c8e175167d1b3d25d7d3374eeb050d0
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-30 15:21:28 -0700

    Update show.blade.php

--- resources/views/hwcategories/show.blade.php
+++ resources/views/hwcategories/show.blade.php
@@ -12,9 +12,9 @@
     
     <h4>
         <div>
-            <a href="{{route('hwcategories.edit', ['manufacturer'=>$hwcategory->id]) }}" class="btn btn-primary" >Edit</a>
-            @csrf
-            <form style="display:inline;" class="delete" action="{{route('hwcategories.destroy', ['hwcategory'=>$hwcategory->id])}}" method="post">
+            <div style="display: inline">
+              <form style="margin: 0; padding: 0" action="{{ route('hwcategories.destroy', ['hwcategory'=>$hwcategory->id]) }}" method="POST">
+                <a class="btn btn-default" href="{{ route('hwcategories.edit', ['hwcategory'=>$hwcategory->id]) }}">Edit</a>
             @method('delete')
             <button type="submit" class="btn btn-danger">Delete</button>
             @csrf
