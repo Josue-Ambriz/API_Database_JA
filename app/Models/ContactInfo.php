b8e2f9742223b8396e0e0105acda840517836f77
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-29 19:54:10 -0700

    Update ContactInfo.php

--- app/Models/ContactInfo.php
+++ app/Models/ContactInfo.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Note;
 
 class ContactInfo extends Model
 {
     use HasFactory;
-    protected $fillable = ['owner', 'info', 'email'];
-
+    protected $fillable = ['owner', 'info', 'email', 'note_id'];
+    
+    public function note()
+    {
+        return $this->belongsTo(Purchase::class);
+    }
 }
