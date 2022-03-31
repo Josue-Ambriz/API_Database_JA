1bd0ef4ef5f6e933cdda46951168a915561adccc
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-30 18:14:58 -0700

    Update 2022_03_27_194435_create_notes_table.php

--- database/migrations/2022_03_27_194435_create_notes_table.php
+++ database/migrations/2022_03_27_194435_create_notes_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
             $table->text('note');
-            $table->foreignId('purchase_id')->constrained('purchases')->onDelete('cascade');
+            $table->foreignId('purchase_id')->nullable()->constrained('purchases')->onUpdate('cascade')->onDelete('cascade');
             $table->string('service');
             $table->timestamps();
         });
