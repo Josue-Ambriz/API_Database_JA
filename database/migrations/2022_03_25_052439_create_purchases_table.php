5e76e01a7ea85cec94f7956bd9e7ca19848f6fbd
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-30 18:12:55 -0700

    Update 2022_03_25_052439_create_purchases_table.php

--- database/migrations/2022_03_25_052439_create_purchases_table.php
+++ database/migrations/2022_03_25_052439_create_purchases_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('invoice');
             $table->float('price');
-            $table->foreignId('hardware_id')->constrained('hardwares')->onDelete('cascade');
-            $table->foreignId('userinfo_id')->constrained('usersinfos')->onDelete('cascade');
+            $table->foreignId('hardware_id')->nullable()->constrained('hardwares')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId('userinfo_id')->nullable()->constrained('usersinfos')->onUpdate('cascade')->onDelete('cascade');
             $table->date('purchased_on');
             $table->timestamps();
         });
