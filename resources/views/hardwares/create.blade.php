23b400d983571ac14c83f5f0d10386199b72ceef
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-30 18:20:49 -0700

    Update create.blade.php

--- resources/views/hardwares/create.blade.php
+++ resources/views/hardwares/create.blade.php
@@ -9,17 +9,13 @@
 @section('content')
 <form method="post" action="{{ route('hardwares.store') }}" >
     @csrf
-    <x-adminlte-input name="name" label="Name of electronic equipment," />
-    <x-adminlte-input name="hwcategory_id" label="Category of electronic equipment," />
-    <x-adminlte-options :options="['Desktop', 'Laptop', 'Phone', 'Tablet', 'Gaming Console']"
-        placeholder="Please choose one,"/>
-    <x-adminlte-input name="cpu" label="CPU Information," />
+    <x-adminlte-input name="name" label="Name of electronic equipment" />
+    <x-adminlte-input name="hwcategory_id" label="Category of electronic equipment" />
+    <x-adminlte-input name="cpu" label="CPU Information" />
     <x-adminlte-input name="ram" label="RAM Information" />
-    <x-adminlte-input name="storage" label="Storage Information," />
-    <x-adminlte-input name="software" label="Software of electronic equipment," />
-    <x-adminlte-options :options="['Orbis OS (Playstation Only)', 'Windows', 'Linux', 'Andriod', 'Apple iOS']"
-        placeholder="Please choose one,"/>
-    <x-adminlte-input name="manufacturer_id" label="Manufacturer Information," />
+    <x-adminlte-input name="storage" label="Storage Information" />
+    <x-adminlte-input name="software" label="Software of Electronic Equipment" />
+    <x-adminlte-input name="manufacturer_id" label="Manufacturer Information" />
     <x-adminlte-button type="Submit" label="Submit" />
 </form>
 @stop
