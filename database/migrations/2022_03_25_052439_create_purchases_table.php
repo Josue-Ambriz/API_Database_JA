1798a55a60d7980e2fffe8e046e91234ca3bc572
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-03-30 18:18:05 -0700

    Update 2022_03_25_052439_create_purchases_table.php

--- database/migrations/2022_03_25_052439_create_purchases_table.php
+++ database/migrations/2022_03_25_052439_create_purchases_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('purchases', function (Blueprint $table) {
             $table->id();
             $table->string('invoice');
-            $table->float('price', $precision = 8, $scale = 2)->nullable();
+            $table->string('price', $precision = 8, $scale = 2)->nullable();
             $table->foreignId('hardware_id')->nullable()->constrained('hardwares')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('userinfo_id')->nullable()->constrained('usersinfos')->onUpdate('cascade')->onDelete('cascade');
             $table->date('purchased_on');
