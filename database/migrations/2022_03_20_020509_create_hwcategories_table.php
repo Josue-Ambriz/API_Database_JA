29775f000335e0a2dd012c8a6d97c40b541c8b02
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-05-03 11:33:12 -0700

    Update 2022_03_20_020509_create_hwcategories_table.php

--- database/migrations/2022_03_20_020509_create_hwcategories_table.php
+++ database/migrations/2022_03_20_020509_create_hwcategories_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('hwcategories', function (Blueprint $table) {
             $table->id();
             $table->string('type');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
