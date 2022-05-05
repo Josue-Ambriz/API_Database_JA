e3a2e3bc2ea93c36fb3254c8db8812b46295e4e2
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-05-04 21:33:42 -0700

    Update 2022_03_24_021702_create_hardwares_table.php

--- database/migrations/2022_03_24_021702_create_hardwares_table.php
+++ database/migrations/2022_03_24_021702_create_hardwares_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('ram')->nullable();
             $table->string('storage')->nullable();
             $table->string('software')->nullable();
+            $table->string('invoice');
             $table->string('price');
             $table->string('purchased_on');
             $table->foreignId('userinfo_id')->constrained();
