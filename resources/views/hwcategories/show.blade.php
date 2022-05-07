f78ae020e13ca10e1647d39096cb6bf3762ca461
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-05-06 20:09:21 -0700

    Update show.blade.php

--- resources/views/hwcategories/show.blade.php
+++ resources/views/hwcategories/show.blade.php
@@ -3,22 +3,71 @@
 @section('title', 'Category Information')
 
 @section('content_header')
-    <h1>Category Information</h1>
+    <a href="{{ route('hardwares.create') }}" class="btn btn-success float-right"> + Add</a>
+    <h1>{{ $hwcategory->type }}</h1>
 @stop
 
 @section('content')
-    <h2>{{ $hwcategory->title; }}</h2>
-    <div><p>{{ $hwcategory->type; }}</p></div>
-    
-    <h4>
-        <div>
-            <div style="display: inline">
-              <form style="margin: 0; padding: 0" action="{{ route('hwcategories.destroy', ['hwcategory'=>$hwcategory->id]) }}" method="POST">
-                <a class="btn btn-default" href="{{ route('hwcategories.edit', ['hwcategory'=>$hwcategory->id]) }}">Edit</a>
-            @method('delete')
-            <button type="submit" class="btn btn-danger">Delete</button>
-            @csrf
-            </form>
-        </div>
-    </h4>
+<div class="row">
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-header p-2">
+        <h3 class="card-title">Hardware</h3>
+      </div>
+      <div class="card-body">
+        <table id="hardware_table" class="table" style="width:100%">
+            <thead>
+                <tr>
+                    <th>Manufacture</th>
+                    <th>Name</th>
+                    <th>CPU</th>
+                    <th>RAM</th>
+                    <th>Storage</th>
+                    <th>Software</th>
+                    <th>Invoice</th>
+                    <th>Price</th>
+                    <th>Purchased On</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($hwcategory->hardware AS $item)
+                <tr>
+                    <td>{{ $item->manufacture->type }}</td>
+                    <td>{{ $item->model }}</td>
+                    <td>{{ $item->CPU }}</td>
+                    <td>{{ $item->memory }}</td>
+                    <td>{{ $item->storage }}</td>
+                    <td>{{ $item->invoice_number }}</td>
+                    <td>{{ $item->price }}</td>
+                    <td>{{ $item->purchase_date }}</td>
+                    <td>
+                      <a class="btn btn-primary btn-xs" href="{{ route('hardware.show',['hardware'=>$item]) }}">View</a>
+                      <form action="{{ route('hardware.destroy',['hardware'=>$item]) }}" method="POST" >
+                        @csrf
+                        <input type="hidden" name="_method" value="DELETE" />
+                        <button type="submit" class="btn btn-danger btn-xs">Delete</button>
+                      </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+
+</div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+    $(document).ready(function() {
+      $('#hardware_table').DataTable();
+    });
+    </script>
 @stop
