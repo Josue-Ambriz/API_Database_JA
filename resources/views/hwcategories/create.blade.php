dca4aeef8cabf0e40ebe3fe36442e8a577d22a07
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-05-06 19:24:41 -0700

    Update create.blade.php

--- resources/views/hwcategories/create.blade.php
+++ resources/views/hwcategories/create.blade.php
@@ -7,10 +7,35 @@
 @stop
 
 @section('content')
-<form method="post" action="{{ route('hwcategories.store') }}" >
-    @csrf
-    <x-adminlte-input name="type" label="Type of Category" />
-    <x-adminlte-button type="Submit" label="Submit" />
-    <a href = class="btn btn-danger float-right">Cancel</a>
-</form>
+<div class="row">
+  <div class="col-md-12">
+    <form action="{{ route('hwcategories.store') }}" method="POST">
+      <div class="card">
+        <div class="card-header p-2">
+          <h3 class="card-title">Infomation</h3>
+        </div>
+        <div class="card-body">
+          @csrf
+          <div class="row">
+              <x-adminlte-input name="name" label="Type of Category" fgroup-class="col-md-12" />
+          </div>
+        </div>
+        <div class="card-footer">
+          <button type="Submit" class="btn btn-primary">Submit</button>
+          <a href="{{ route('hwcategories.index') }}" class="btn btn-danger float-right">Cancel</a>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+    
+    </script>
 @stop
