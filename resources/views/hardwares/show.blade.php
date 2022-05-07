dbc4e9b8ab735fee614477a8d032d32df7a37b5c
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-05-06 21:11:33 -0700

    Update show.blade.php

--- resources/views/hardwares/show.blade.php
+++ resources/views/hardwares/show.blade.php
@@ -7,24 +7,117 @@
 @stop
 
 @section('content')
-    <h2>{{ $hardware->title; }}</h2>
-    <div><p>{{ $hardware->name; }}</p></div>
-    <div><p>{{ $hardware->hwcategory_id; }}</p></div>
-    <div><p>{{ $hardware->cpu; }}</p></div>
-    <div><p>{{ $hardware->ram; }}</p></div>
-    <div><p>{{ $hardware->storage; }}</p></div>
-    <div><p>{{ $hardware->software; }}</p></div>
-    <div><p>{{ $hardware->manufacturer_id; }}</p></div>
-    
-    <h4>
-        <div>
-            <a href="{{route('hardwares.edit', ['hardware'=>$hardware->id]) }}" class="btn btn-primary" >Edit</a>
-            @csrf
-            <form style="display:inline;" class="delete" action="{{route('hardwares.destroy', ['hardware'=>$hardware->id])}}" method="post">
-            @method('delete')
-            <button type="submit" class="btn btn-danger">Delete</button>
-            @csrf
-            </form>
+<div class="row">
+  <div class="col-md-3">
+    <div class="card card-primary">
+      <div class="card-header">
+        <h3 class="card-title">Details</h3>
+      </div>
+      <div class="card-body">
+        <strong><i class="fas fa-book mr-1"></i> Hardware Specification</strong>
+        <p class="text-muted">
+          CPU: {{ $hardware->CPU }}<br>
+          RAM: {{ $hardware->memory }}<br>
+          Storage{{ $hardware->storage }}
+        </p>
+        <hr>
+        <strong><i class="far fa-file-alt mr-1"></i> Purchase Info</strong>
+        <p class="text-muted">
+          Invoice#: {{ $hardware->invoice_number }}<br>
+          Price: $ {{ $hardware->price }}<br>
+          Purchase Date: {{ $hardware->purchase_date }}
+        </p>
+      </div>
+      <div class="card-footer">
+        <a class="btn btn-success btn-block" href="{{ route('hardwares.edit',['hardware'=>$hardware]) }}">Edit</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-9">
+    <div class="card card-warning">
+      <div class="card-header">
+        <h3 class="card-title">Notes</h3>
+        <div class="card-tools">
+        <a href="{{ route('notes.create',['hardware'=>$hardware->id]) }}" class="btn btn-tool" ><i class="fas fa-plus"></i> Add</a>
         </div>
-    </h4>
+      </div>
+      <div class="card-body p-0">
+        <table id="note_table" class="table table-sm" style="width:100%">
+            <thead>
+                <tr>
+                    <th>Subject</th>
+                    <th>Content</th>
+                    <th>Create Date</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($hardware->notes AS $note)
+                <tr>
+                    <td>{{ $note->note }}</td>
+                    <td>{{ $note->service }}</td>
+                    <td>
+                      <a class="btn btn-primary btn-xs" href="{{ route('notes.edit',['note'=>$note->id]) }}">Edit</a>
+                      <form action="{{ route('notes.destroy',['note'=>$note->id]) }}" method="POST" >
+                        @csrf
+                        <input type="hidden" name="_method" value="DELETE" />
+                        <button type="submit" class="btn btn-danger btn-xs">Delete</button>
+                      </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+      </div>
+    </div>
+
+    <div class="card  card-primary">
+      <div class="card-header">
+        <h3 class="card-title">User Associated</h3>
+      </div>
+      <div class="card-body p-0">
+        <table id="userinfo_table" class="table table-sm" style="width:100%">
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($hardware->usersinfos AS $userinfo)
+                <tr>
+                    <td>{{ $userinfo->first_name }}</td>
+                    <td>{{ $userinfo->last_name }}</td>
+                    <td>{{ $userinfo->email }}</td>
+                    <td>{{ $userinfo->phone }}</td>
+                    <td><a class="btn btn-primary btn-xs" href="{{ route('usersinfos.show',['userinfo'=>$userinfo->id]) }}">View</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+
+</div>
+@stop
+
+@section('css')
+
+@stop
+
+@section('js')
+    <script>
+    $(document).ready(function() {
+      $('.table').DataTable({
+        "paging":   false,
+        "info":     false,
+        "searching": false
+      });
+    });
+    </script>
 @stop
