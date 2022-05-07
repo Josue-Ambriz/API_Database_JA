5bfd5de96c4854575cf5f6dd7f2b2ee79d5e8abd
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-05-06 19:27:17 -0700

    Update edit.blade.php

--- resources/views/hwcategories/edit.blade.php
+++ resources/views/hwcategories/edit.blade.php
@@ -7,11 +7,36 @@
 @stop
 
 @section('content')
-<form method="post" action="{{ route('hwcategories.update', ['hwcategory'=>$hwcategory->id]) }}">
-  @csrf
-  @method('PUT')
-  <x-adminlte-input name="type" value="{{ $hwcategory->type }}" label="Type of Category" />
-  <x-adminlte-button type="Submit" label="Submit" />
-  <a href = class="btn btn-danger float-right">Cancel</a>
-</form>
+<div class="row">
+  <div class="col-md-12">
+    <form action="{{ route('hwcategories.update', ['hwcategory'=>$hwcategory]) }}" method="POST">
+      <div class="card">
+        <div class="card-header p-2">
+          <h3 class="card-title">Infomation</h3>
+        </div>
+        <div class="card-body">
+          @csrf
+          <input type="hidden" name="_method" value="PUT" />
+          <div class="row">
+              <x-adminlte-input name="name" label="Type of Category" fgroup-class="col-md-12" value="{{ old('name')?:$category->name }}" />
+          </div>
+        </div>
+        <div class="card-footer">
+          <button type="Submit" class="btn btn-primary">Submit</button>
+          <a href="{{ route('hwcategories.show',['hwcategory'=>$hwcategory]) }}" class="btn btn-danger float-right">Cancel</a>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+    
+    </script>
 @stop
