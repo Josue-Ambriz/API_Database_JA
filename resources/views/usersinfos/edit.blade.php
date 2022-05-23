5d85d8c03a97907cad12e4bdc77ca8da7fd8dbef
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-05-22 19:11:39 -0700

    Update edit.blade.php

--- resources/views/usersinfos/edit.blade.php
+++ resources/views/usersinfos/edit.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 <div class="row">
   <div class="col-md-12">
-    <form action="{{ route('usersinfos.update',['user'=>$userinfo]) }}" method="POST">
+    <form action="{{ route('usersinfos.update',['userinfo'=>$userinfo]) }}" method="POST">
       <div class="card">
         <div class="card-header p-2">
           <h3 class="card-title">User Information</h3>
@@ -18,13 +18,15 @@
           @csrf
           <input type="hidden" name="_method" value="PUT" />
           <div class="row">
-              <x-adminlte-input name="name" label="User Name" fgroup-class="col-md-12" value="{{ old('name')?:$user->name }}" />
-              <x-adminlte-input name="email" label="Email" fgroup-class="col-md-12" value="{{ old('email')?:$user->email }}" />
+              <x-adminlte-input name="first_name" label="First Name" fgroup-class="col-md-12" value="{{ old('first_name')?:$userinfo->first_name }}" />
+              <x-adminlte-input name="last_name" label="Last Name" fgroup-class="col-md-12" value="{{ old('last_name')?:$userinfo->last_name }}" />
+              <x-adminlte-input name="email" label="Email" fgroup-class="col-md-12" value="{{ old('email')?:$userinfo->email }}" />
+              <x-adminlte-input name="phone" label="Phone" fgroup-class="col-md-12" value="{{ old('phone')?:$userinfo->phone }}" />
           </div>
         </div>
         <div class="card-footer">
           <button type="Submit" class="btn btn-primary float-right">Submit</button>
-          <a href="{{ route('users.show',['user'=>$user]) }}" class="btn btn-danger">Cancel</a>
+          <a href="{{ route('usersinfos.show',['userinfo'=>$userinfo]) }}" class="btn btn-danger">Cancel</a>
         </div>
       </div>
     </form>
