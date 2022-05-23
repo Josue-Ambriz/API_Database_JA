22517323aa99222e45dd67df95339b9736546757
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-05-22 18:41:32 -0700

    Update edit.blade.php

--- resources/views/notes/edit.blade.php
+++ resources/views/notes/edit.blade.php
@@ -7,12 +7,69 @@
 @stop
 
 @section('content')
-<form method="post" action="{{ route('notes.update', ['note'=>$note->id]) }}">
-  @csrf
-  @method('PUT')
-  <x-adminlte-input name="note" value="{{ $note->note }}" label="Edit Note" />
-  <x-adminlte-input name="purchase_id" value="{{ $note->purchase_id }}" label="Edit Purchase ID" />
-  <x-adminlte-input name="service" value="{{ $note->service }}" label="Edit Service" />
-  <x-adminlte-button type="Submit" label="Submit" />
-</form>
+<div class="row">
+  <div class="col-md-3">
+    <div class="card card-primary">
+      <div class="card-header">
+        <h3 class="card-title">Details</h3>
+      </div>
+      <div class="card-body">
+        <strong><i class="fas fa-book mr-1"></i> Hardware Specs</strong>
+        <p class="text-muted">
+          CPU: {{ $hardware->CPU }}<br>
+          RAM: {{ $hardware->RAM }}<br>
+          Storage{{ $hardware->storage }}
+          Service{{ $hardware->storage }}
+        </p>
+        <hr>
+        <strong><i class="far fa-file-alt mr-1"></i> Purchase Info</strong>
+        <p class="text-muted">
+          Invoice: {{ $hardware->invoice }}<br>
+          Price: $ {{ $hardware->price }}<br>
+          Purchase Date: {{ $hardware->purchased_on }}
+        </p>
+      </div>
+      <div class="card-footer">
+        <a class="btn btn-success btn-block" href="{{ route('hardwares.edit',['hardware'=>$hardware]) }}">Edit</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-md-9">
+    <div class="card card-warning">
+      <form action="{{ route('notes.update',['note'=>$note->id]) }}" method="POST">
+        <div class="card-header">
+          <h3 class="card-title">Edit Note</h3>
+        </div>
+        <div class="card-body">
+          @csrf          
+          <input type="hidden" name="_method" value="PUT" />
+          <x-adminlte-input name="note" type="text" placeholder="note" value="{{ old('note')?old('note'):$note->note }}" />
+          <x-adminlte-textarea name="service" placeholder="Service ...">{{ old('Service')?old('Service'):$note->service }}</x-adminlte-textarea>
+        </div>
+        <div class="card-footer">
+          <button type="submit" class="btn btn-success">Submit</button>
+        </div>
+      </form>
+    </div>
+
+  </div>
+
+</div>
+@stop
+
+@section('css')
+
+@stop
+
+@section('js')
+    <script>
+    $(document).ready(function() {
+      $('.table').DataTable({
+        "paging":   false,
+        "info":     false,
+        "searching": false
+      });
+    });
+    </script>
 @stop
