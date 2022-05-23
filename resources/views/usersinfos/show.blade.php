6f7944c41b0b2051476caa1b04d6295d7121602b
Josue-Ambriz <99371776+Josue-Ambriz@users.noreply.github.com>
2022-05-22 19:20:38 -0700

    Update show.blade.php

--- resources/views/usersinfos/show.blade.php
+++ resources/views/usersinfos/show.blade.php
@@ -3,25 +3,78 @@
 @section('title', 'User Information')
 
 @section('content_header')
-    <h1>User Information</h1>
+    <a href="{{ route('usersinfos.edit',['userinfo'=>$userinfo]) }}" class="btn btn-primary float-right"> Edit</a>
+    <h1>{{ $userinfo->first_name }}</h1>
+    <p>{{ $userinfo->last_name }} </p>
+    <p>{{ $user->email }} </p>
+    <p>{{ $user->phone }} </p>
 @stop
 
 @section('content')
-    <h2>{{ $userinfo->title; }}</h2>
-    <div><p>{{ $userinfo->first_name; }}</p></div>
-    <div><p>{{ $userinfo->last_name; }}</p></div>
-    <div><p>{{ $userinfo->email; }}</p></div>
-    <div><p>{{ $userinfo->phone; }}</p></div>
-    
-    <h4>
-        <div>
-            <a href="{{route('usersinfos.edit', ['usersinfo'=>$userinfo->id]) }}" class="btn btn-primary" >Edit</a>
-            @csrf
-            <form style="display:inline;" class="delete" action="{{route('usersinfos.destroy', ['usersinfo'=>$userinfo->id])}}" method="post">
-            @method('delete')
-            <button type="submit" class="btn btn-danger">Delete</button>
-            @csrf
-            </form>
-        </div>
-    </h4>
+<div class="row">
+  <div class="col-md-12">
+    <div class="card">
+      <div class="card-header p-2">
+        <a href="{{ route('hardwares.create') }}" class="btn btn-success float-right"> + Add Hardware</a>
+        <h3 class="card-title">Hardware</h3>
+      </div>
+      <div class="card-body">
+        <table id="hardware_table" class="table" style="width:100%">
+            <thead>
+                <tr>
+                    <th>Manufacturer</th>
+                    <th>Category</th>
+                    <th>Name</th>
+                    <th>CPU</th>
+                    <th>RAM</th>
+                    <th>Storage</th>
+                    <th>Software</th>
+                    <th>Invoice</th>
+                    <th>Price</th>
+                    <th>Purchased On</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($userinfo->hardware AS $item)
+                <tr>
+                    <td>{{ $item->manufacturer->name }}</td>
+                    <td>{{ $item->hwcategory->name }}</td>
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->CPU }}</td>
+                    <td>{{ $item->RAM }}</td>
+                    <td>{{ $item->storage }}</td>
+                    <td>{{ $item->software }}</td>
+                    <td>{{ $item->invoice }}</td>
+                    <td>{{ $item->price }}</td>
+                    <td>{{ $item->purchased_on }}</td>
+                    <td>
+                      <a class="btn btn-primary btn-xs" href="{{ route('hardwares.show',['hardware'=>$item]) }}">View</a>
+                      <form action="{{ route('hardwares.destroy',['hardware'=>$item]) }}" method="POST" >
+                        @csrf
+                        <input type="hidden" name="_method" value="DELETE" />
+                        <button type="submit" class="btn btn-danger btn-xs">Delete</button>
+                      </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+
+</div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script>
+    $(document).ready(function() {
+      $('#hardware_table').DataTable();
+    });
+    </script>
 @stop
